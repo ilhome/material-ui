8486ec8939844ad6c14d4016bf142c727a38b4ff
ilhome <ilkhom9799@gmail.com>
2021-10-29 10:33:15 +0500

    some changes

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import SideMenu from "../components/SideMenu";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <SideMenu />
+      <div>here we go</div>
+    </>
+  );
+}
+
+export default App;
