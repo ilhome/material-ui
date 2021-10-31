43e2912549d0c33df36b5d70cb5695fbc2d50590
ilhome <ilkhom9799@gmail.com>
2021-10-31 12:19:25 +0500

    almost ready

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,44 @@
-import { makeStyles } from "@material-ui/styles";
 import React from "react";
-import SideMenu from "../components/SideMenu";
 import "./App.css";
+import SideMenu from "../components/SideMenu";
+import {
+  makeStyles,
+  CssBaseline,
+  createMuiTheme,
+  ThemeProvider,
+} from "@material-ui/core";
+import Header from "../components/Header";
+// import PageHeader from "../components/PageHeader";
+
+import Employees from "../pages/Employees/Employees";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#333996",
+      light: "#3c44b126",
+    },
+    secondary: {
+      main: "#f83245",
+      light: "#f8324526",
+    },
+    background: {
+      default: "#f4f5fd",
+    },
+  },
+  overrides: {
+    MuiAppBar: {
+      root: {
+        transform: "translateZ(0)",
+      },
+    },
+  },
+  props: {
+    MuiIconButton: {
+      disableRipple: true,
+    },
+  },
+});
 
 const useStyles = makeStyles({
   appMain: {
@@ -12,11 +49,17 @@ const useStyles = makeStyles({
 
 function App() {
   const classes = useStyles();
+
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <SideMenu />
-      <div className={classes.appMain}>here we go</div>
-    </>
+      <div className={classes.appMain}>
+        <Header />
+
+        <Employees />
+      </div>
+      <CssBaseline />
+    </ThemeProvider>
   );
 }
 
--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { makeStyles, withStyles } from "@material-ui/core";
 
-const useStyles = makeStyles({
+// withStyles & makeStyles
+
+const style = {
   sideMenu: {
     display: "flex",
     flexDirection: "column",
@@ -11,10 +13,11 @@ const useStyles = makeStyles({
     height: "100%",
     backgroundColor: "#253053",
   },
-});
-const SideMenu = () => {
-  const classes = useStyles();
-  return <div className={classes.sideMenu}>help</div>;
 };
 
-export default SideMenu;
+const SideMenu = (props) => {
+  const { classes } = props;
+  return <div className={classes.sideMenu}></div>;
+};
+
+export default withStyles(style)(SideMenu);
